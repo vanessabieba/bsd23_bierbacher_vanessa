54e75cec51f77355610ebb9dd253cd9c762e49c7
Vanessa Bierbacher <s52701@edu.campus02.at>
2024-05-02 12:54:33 +0200

    uebung3 calculator

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,2 +1,16 @@
-package at.campus02.bsd;public class Calculator {
+package at.campus02.bsd;
+
+public class Calculator {
+    public static double add(double number1, double number2){
+        return number1 + number2;
+    }
+    public static double minus(double number1, double number2){
+        return number1 - number2;
+    }
+    public static double divide(double number1, double number2){
+        return number1 / number2;
+    }
+    public static double multiply(double number1, double number2){
+        return number1 * number2;
+    }
 }
